fe9d337104ef3ad3ce6871b1b17118c965b91963
Kyle <kylejmbaylis@gmail.com>
2022-03-12 20:00:58 -0500

    adding tasks works properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,26 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
-
+    // stops browser from using default setting for things like the dropdown menu
     event.preventDefault();
-
+    // takes the input from the 'task-name' box and stores it in the variable (can also check console.dir)
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    // takes input from task type dropdown and stores it in this variable
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // create a list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
+    
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
